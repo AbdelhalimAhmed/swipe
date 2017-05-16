accda017690cec968deb042d327ad9c50e556dd8
AbdelhalimAhmed <abdelhalim.ahmed95@gmail.com>
2017-05-16 12:53:15 +0200

    Add animating to view card and move it by PanResponder

--- src/Deck.js
+++ src/Deck.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'React';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 class Deck extends React.Component {
-
+    constructor(props) {
+        super(props);
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () =>  true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x:  gesture.dx , y: gesture.dy });
+            },
+            onPanResponderRelease: () => {}
+        });
+        this.state = { panResponder, position   }
+    }
     renderCards() {
         return this.props.data.map(item => {
             return this.props.renderCard(item)
@@ -10,9 +21,11 @@ class Deck extends React.Component {
     }
     render() {
         return(
-            <View >
+            <Animated.View 
+                style={this.state.position.getLayout()}
+                {...this.state.panResponder.panHandlers}>
                 {this.renderCards()}
-            </View>
+            </Animated.View>
         );
     }
 }
